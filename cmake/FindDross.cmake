0b25a2a656f5deae894dd994a68c24711ba50081
Yuma Endo <endo@skipbit.jp>
2024-10-06 16:22:24 +0900

    [WIP] Build without submodules

---
+++ cmake/FindDross.cmake
@@ -0,0 +1,21 @@
+include(ExternalProject)
+
+ExternalProject_Add(dross
+    GIT_REPOSITORY https://github.com/skipbit/dross.git
+    GIT_TAG main
+    PREFIX ${CMAKE_BINARY_DIR}/dependencies/dross
+    CMAKE_ARGS
+        -DCMAKE_BUILD_TYPE=$<CONFIG>
+        -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}
+    BUILD_ALWAYS FALSE
+)
+
+set(DROSS_INCLUDE_DIR ${CMAKE_BINARY_DIR}/include)
+set(DROSS_LIBRARY_DIR ${CMAKE_BINARY_DIR}/lib)
+
+add_library(dross_library SHARED IMPORTED)
+set_target_properties(dross_library PROPERTIES
+    IMPORTED_LOCATION ${DROSS_LIBRARY_DIR}/libdross.dylib
+)
+
+add_dependencies(dross_library dross)
